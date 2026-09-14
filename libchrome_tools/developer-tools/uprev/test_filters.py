8ff98f6c8283bb550b67767193bf24b396f51c90
Di Wu <diwux@google.com>
2026-09-14 03:10:30 +0000

    libchrome: Exclude all upstream OWNERS files during uprev
    Exclude all OWNERS file variations when importing upstream Chromium
    base files, and delete leaked METRICS_OWNERS and MIRACLE_PTR_OWNERS.
    
    Upstream OWNERS files are unused in libchrome and cause cleanup bots
    to flag upstream owners as inactive in ChromeOS.
    
    BUG=b:560929133
    TEST=cd libchrome_tools/developer-tools/uprev && python3 -m unittest test_filters.py
    
    Change-Id: Ie64576a96277c7b5b78181b3e353d7447544ac64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/libchrome/+/8404506
    Tested-by: Di Wu <diwux@google.com>
    Reviewed-by: Qijiang Fan <fqj@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Georg Neis <neis@chromium.org>

--- libchrome_tools/developer-tools/uprev/test_filters.py
+++ libchrome_tools/developer-tools/uprev/test_filters.py
@@ -6,6 +6,7 @@
 import re
 import unittest
 
+import filter_config
 import filters
 import utils
 
@@ -18,28 +19,60 @@ class TestFilters(unittest.TestCase):
         ]
 
     def test_filters(self):
-        test_filter = filters.Filter([re.compile(rb'want.*')],
-                                     [re.compile(rb'want_excluded.*')],
-                                     [re.compile(rb'want_excluded_always.*')])
+        test_filter = filters.Filter(
+            [re.compile(rb"want.*")],
+            [re.compile(rb"want_excluded.*")],
+            [re.compile(rb"want_excluded_always.*")],
+        )
 
-        self.assertEquals(
+        self.assertEqual(
             test_filter.filter_files(
-                self._build_file_list([
-                    'want/xxx',
-                    'want_excluded/xxx',
-                    'want_excluded_always/xxx',
-                    'unrelated_upstream_file',
-                ])),
-            self._build_file_list(
-                ['want/xxx', 'want_excluded_always/xxx']),
+                self._build_file_list(
+                    [
+                        "want/xxx",
+                        "want_excluded/xxx",
+                        "want_excluded_always/xxx",
+                        "unrelated_upstream_file",
+                    ]
+                )
+            ),
+            self._build_file_list(["want/xxx", "want_excluded_always/xxx"]),
         )
 
     def test_path_filter(self):
-        test_filter = filters.Filter([filters.PathFilter([b'a/b/c', b'd'])], [],
-                                     [])
+        test_filter = filters.Filter(
+            [filters.PathFilter([b"a/b/c", b"d"])], [], []
+        )
 
-        self.assertEquals(
+        self.assertEqual(
             test_filter.filter_files(
                 self._build_file_list(
-                    ['a', 'b', 'c', 'a/b', 'b/c', 'a/b/c', 'd'])),
-            self._build_file_list(['a/b/c', 'd']))
+                    ["a", "b", "c", "a/b", "b/c", "a/b/c", "d"]
+                )
+            ),
+            self._build_file_list(["a/b/c", "d"]),
+        )
+
+    def test_filter_config_owners(self):
+        libchrome_filter = filters.Filter(
+            filter_config.WANT,
+            filter_config.WANT_EXCLUDE,
+            filter_config.ALWAYS_WANT,
+        )
+
+        # OWNERS files and variations should be excluded.
+        self.assertFalse(libchrome_filter.want_file(b"base/OWNERS"))
+        self.assertFalse(
+            libchrome_filter.want_file(b"base/metrics/METRICS_OWNERS")
+        )
+        self.assertFalse(
+            libchrome_filter.want_file(b"base/memory/MIRACLE_PTR_OWNERS")
+        )
+        self.assertFalse(libchrome_filter.want_file(b"base/SECURITY_OWNERS"))
+        self.assertFalse(libchrome_filter.want_file(b"base/OWNERS.android"))
+
+        # Files that are not owners should be kept.
+        self.assertTrue(
+            libchrome_filter.want_file(b"base/metrics/histogram.cc")
+        )
+        self.assertTrue(libchrome_filter.want_file(b"base/OWNERS.x/foo.cc"))
